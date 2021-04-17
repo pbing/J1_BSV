aad0735d83a928a5095ef5524501b33898ac0b4c
Bernd Beuster <bernd.beuster@gmail.com>
2021-04-17 21:20:04 +0200

    Simplify

--- env.sh
+++ env.sh
@@ -5,10 +5,7 @@ SIMDIR=simdir
 
 mkdir -p $BDIR $VDIR $SIMDIR
 
-export RTL_OPTIONS="-opt-undetermined-vals -unspecified-to X"
-
 export BSC_OPTIONS="-p src:test:+ -bdir $BDIR -vdir $VDIR -simdir $SIMDIR -o bsim"
 
 # uncomment for better RTL synthesis results
-#export BSC_OPTIONS="$BSC_OPTIONS $RTL_OPTIONS"
-
+#export BSC_OPTIONS="$BSC_OPTIONS -opt-undetermined-vals -unspecified-to X"
